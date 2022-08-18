c27f02078c302a201c70b0987431ed462b8c6b6e
mrchristian17 <dcwrath17@gmail.com>
2022-08-18 17:35:37 -0600

    Added prisma db implementation to get blog posts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,13 +1,14 @@
+import { prisma } from "~/db.server";
+
+export async function getPostListings() {
+  return prisma.post.findMany({
+    select: {
+      slug: true,
+      title: true,
+    },
+  });
+}
+
 export async function getPosts() {
-    const posts = [
-        {
-            slug: 'my-first-post',
-            title: 'My First Post',
-        },
-        {
-            slug: 'trail-riding-with-onewheel',
-            title: 'Trail Riding with Onewheel',
-        }
-    ]
-    return posts;
+    return prisma.post.findMany();
 }
