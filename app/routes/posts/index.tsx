e0b74c87efb14bdeb41752a6f3f6ba024383724c
mrchristian17 <dcwrath17@gmail.com>
2022-08-19 13:41:23 -0600

    Added admin route

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -4,26 +4,31 @@ import type { LoaderFunction } from "@remix-run/node";
 import { getPostListings } from "~/models/post.server";
 
 type LoaderData = {
-    posts: Awaited<ReturnType<typeof getPostListings>>,
-}
+  posts: Awaited<ReturnType<typeof getPostListings>>;
+};
 
 export const loader: LoaderFunction = async () => {
-    const posts = await getPostListings();
-    return json<LoaderData>({posts});
-}
+  const posts = await getPostListings();
+  return json<LoaderData>({ posts });
+};
 
-export default function PostsRoute (){
-    const { posts } = useLoaderData() as LoaderData;
+export default function PostsRoute() {
+  const { posts } = useLoaderData() as LoaderData;
   return (
     <main>
-        <h1>Posts</h1>
-        <ul>
-            {posts.map(post => (
-                <li key={post.slug}>
-                    <Link to={post.slug} className="text-blue-600 underline">{post.title}</Link>
-                </li>
-            ))}
-        </ul>
+      <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
